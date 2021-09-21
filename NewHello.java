1a707533edd7f850c328527825030e1fa4c0dced
unknown <mrdnyaneshwarkamble@gmail.com>
2021-09-21 17:29:04 +0530

    commitng all the file

---
+++ NewHello.java
@@ -0,0 +1,7 @@
+public class NewHello{
+
+
+	public static void main(String[] args) {
+system.out.println("Hello")
+}
+}
---
+++ NewHello1.java
@@ -0,0 +1,7 @@
+public class NewHello1{
+
+
+	public static void main(String[] args) {
+system.out.println("Hello1")
+}
+}
